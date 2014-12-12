5c3e90f4e58325ae2ca4b9f5be034a54af2c84ce
Denffer <denffer18@gmail.com>
2014-12-12 19:05:50 +0000

    I want to contributegit add . 3:05

--- polls/admin.py
+++ polls/admin.py
@@ -7,8 +7,8 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,  {'fields': [question_text]}),
         }
         
-        
-        
-#This is a testing to committing to githu
+#This is a testing to committing to github
+#This is a testing to committing to github
+#This is a testing to committing to github
 
 admin.site.register(Question, QuestionAdmin)
